6e4ba180160bc34e1d4f311341f72089c51f9bf9
Imakshat47 <imakshat47@gmail.com>
2021-04-17 23:28:54 +0530

    Update

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
     try:
         # Data Collection & Cleaning        
         tweets = t.Tweets()
+        tweets._model()
         tweets._fetch()        
         tweets._model()
     except Exception as e:
+        tweets._model()
         print("App Error:   ", e)        
         pass
     exit("App Closes")
