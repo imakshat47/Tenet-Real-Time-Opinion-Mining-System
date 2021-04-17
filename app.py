0f4c7a0b7f18e9008c16a6af80721f1c38b30d3e
Imakshat47 <imakshat47@gmail.com>
2021-04-17 23:45:26 +0530

    Update

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import src.Tweets as t
 # Driver Function
 if __name__ == '__main__':
     try:
-        # Data Collection & Cleaning        
         tweets = t.Tweets()
-        tweets._model()
-        tweets._fetch()        
+        # Data Collection & Cleaning
+        tweets._fetch()
         tweets._model()
     except Exception as e:
         tweets._model()
-        print("App Error:   ", e)        
+        print("App Error:  ", e)
         pass
-    exit("App Closes")
+    print("App Closes")
