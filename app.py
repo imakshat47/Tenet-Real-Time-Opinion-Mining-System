1adf289b593e3fd8a026b38aedc78a29d7213d63
Imakshat47 <imakshat47@gmail.com>
2021-04-22 01:46:23 +0530

    Updates: Results Scores and Double Working

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
         tweets = t.Tweets()
         # Data Collection & Cleaning
         tweets._model({'polarity': {'$exists': False}})                 # to be removed in near future
-        # tweets._fetch()
-        # tweets._model()
+        tweets._fetch()
+        tweets._model()
     except Exception as e:
         tweets._model({'polarity': {'$exists': False}})
         print("App Error:  ", e)        
