167eb8d5144877699dd88a561593e9a457c0eb94
Imakshat47 <imakshat47@gmail.com>
2021-05-03 22:11:52 +0530

    Updates: Sleep Checks:

--- Model.py
+++ Model.py
@@ -42,6 +42,7 @@ class Model(object):
                     threads = []
             except:
                 print("Error here => ", e)
+                print("Going to sleep...")
                 sleep(120)
                 continue
         for thread in threads:
@@ -59,19 +60,27 @@ class Model(object):
     # middleware for heroku
     def __middleware(self, text, lang_tag, id):
         print("-- Thread Set --")
+        print("Going to sleep...")
         sleep(key._sleep_time)
+        print("Back from sleep...")
         print("Thread Active Now!!")
         # result
         polarity = self.sa._score(text)
+        print("Going to sleep...")
         sleep(key._sleep_time)
+        print("Back from sleep...")
         trans_text = self.trans_module._translate(text, lang_tag)
+        print("Going to sleep...")
         sleep(key._sleep_time)
+        print("Back from sleep...")
         trans_polarity = self.sa._score(trans_text)
         set_data = {"$set": {"trans_text": trans_text,
                              "polarity": polarity, "trans_polarity": trans_polarity}}
         print(set_data)
         self.__db._update({"_id": id}, set_data)
+        print("Going to sleep...")
         sleep(key._sleep_time)
+        print("Back from sleep...")
         # Update Result
         _obj = self._db._find({"_id": key._tenet_record})
         if _obj != None:
